0b8c1815c3b13833060488ffcdc3d12edec47330
rodrigo23 <rodrigo23@163.com>
2012-08-15 11:22:01 +0800

    Signed-off-by: rodrigo23 <rodrigo23@163.com>

---
+++ pythonspace/MyPython/tangcan/main.py
@@ -0,0 +1,6 @@
+import os
+import fileUtil
+
+#__author__ = 'TC'
+fileUtil.unzip_file("x:/apks/decoded/com.ireadercity.b3_102904/res/raw/b210.epub", "x:/apks/decoded/com.ireadercity.b3_102904/res/raw/b210");
+
